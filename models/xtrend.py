17d161494fd9305d395a4f54513789fd299df1f2
Zayd <zaydbenfathel@gmail.com>
2025-02-15 12:03:32 +0100

    FInal commit

--- models/xtrend.py
+++ models/xtrend.py
@@ -13,6 +13,7 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+options.add_argument("--headless")  # Comment this line to see the browser in action
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
